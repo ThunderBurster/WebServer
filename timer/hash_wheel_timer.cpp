cf4523787071a6948aadce073309fee4fe778238
DL <21921009@zju.edu.cn>
2021-06-20 17:16:54 +0800

    HashWheelTimer done

--- timer/hash_wheel_timer.cpp
+++ timer/hash_wheel_timer.cpp
@@ -4,6 +4,7 @@
 
 HashWheelTimer::HashWheelTimer(int intervalMs) {
     m_circleIntervalMs = intervalMs;
+    m_pBlock = 0;
 }
 
 HashWheelTimer::~HashWheelTimer() {
@@ -11,15 +12,58 @@ HashWheelTimer::~HashWheelTimer() {
 }
 
 bool HashWheelTimer::addFd(int fd, int timeOutMs) {
-    // to do
+    int nBlocks = timeOutMs / m_circleIntervalMs;
+    int round = nBlocks / CIRCLE_LEN;
+    m_mutex.lock();
+    // pBlock may change
+    // lock after get pos, pBlock tick, a more round time
+    int pos = (m_pBlock + nBlocks % CIRCLE_LEN) % CIRCLE_LEN;
+    if(!m_fd2Block.count(fd)) {
+        m_circle[pos].push_front(std::make_pair(fd, round));
+        m_fd2Block[fd] = pos;
+        m_fd2It[fd] = m_circle[pos].begin();
+        m_mutex.unlock();
+        return true;
+    }
+    else {
+        // fd already exist
+        m_mutex.unlock();
+        return false;
+    }
 }
 
 bool HashWheelTimer::removeFd(int fd) {
-    // to do
+    m_mutex.lock();
+    if(m_fd2Block.count(fd)) {
+        m_circle[m_fd2Block[fd]].erase(m_fd2It[fd]);
+        m_fd2Block.erase(fd);
+        m_fd2It.erase(fd);
+        m_mutex.unlock();
+        return true;
+    }
+    else {
+        m_mutex.unlock();
+        return false;
+    }
 }
 
-std::vector<int> tick() {
-    // to do
+std::vector<int> HashWheelTimer::tick() {
+    // invoked every m_circleIntervalMs ms
+    m_mutex.lock();
+    std::vector<int> ret;
+    auto it = m_circle[m_pBlock].begin();
+    while(it != m_circle[m_pBlock].end()) {
+        if(it->second > 0) {
+            it->second --;
+            it ++;
+        }
+        else {
+            ret.push_back(it->first);
+            it = m_circle->erase(it);
+        }
+    }
+    m_mutex.unlock();
+    return ret;
 }
 
 
--- timer/hash_wheel_timer.h
+++ timer/hash_wheel_timer.h
@@ -12,9 +12,11 @@ public:
     const static int CIRCLE_LEN = 512;
 private:
     int m_circleIntervalMs;
+    int m_pBlock;
     Mutex m_mutex;
-    std::unordered_map<int, int> m_circle[CIRCLE_LEN];  // fd to round
-    std::unordered_map<int, int> m_fd2Block;  // fd to which block
+    std::list<std::pair<int, int>> m_circle[CIRCLE_LEN];  // pair: fd->round
+    std::unordered_map<int, int> m_fd2Block;  // fd 2 which block
+    std::unordered_map<int, std::list<std::pair<int, int>>::iterator> m_fd2It;  // fd to iterator
 public:
     HashWheelTimer(int intervalMs);
     ~HashWheelTimer();
